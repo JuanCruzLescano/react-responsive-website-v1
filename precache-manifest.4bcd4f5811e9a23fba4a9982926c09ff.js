4a8078d39f6c0be5c188aa485441e9536fdb1d6b
JuanCruzLescano <lesca5172@outlook.com>
2020-10-23 01:11:05 -0300

    Updates

---
+++ precache-manifest.4bcd4f5811e9a23fba4a9982926c09ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea07ab7bb3efaea920e3637e52d913ff",
+    "url": "/react-responsive-website-v1/index.html"
+  },
+  {
+    "revision": "9792bdb9051894d30977",
+    "url": "/react-responsive-website-v1/static/css/main.4704c06f.chunk.css"
+  },
+  {
+    "revision": "3609b726974633381b7f",
+    "url": "/react-responsive-website-v1/static/js/2.bbbd2f0f.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/react-responsive-website-v1/static/js/2.bbbd2f0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9792bdb9051894d30977",
+    "url": "/react-responsive-website-v1/static/js/main.098ca2be.chunk.js"
+  },
+  {
+    "revision": "6e626b3f76f716399255",
+    "url": "/react-responsive-website-v1/static/js/runtime-main.60b18f4f.js"
+  }
+]);
